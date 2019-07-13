79761e9af17d83d9a1578876f77f992918e760c0
Josh Tscheschlog <jtscheschlog@kbra.com>
2019-07-13 19:32:41 -0400

    feat: update config.py - fider and auth0

--- src/config/config.py
+++ src/config/config.py
@@ -2,8 +2,13 @@
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-slack_api = {
-    "webhook_url": str(os.getenv("SLACK_WEBHOOK_URL"))
+auth0 = {
+    "domain": str(os.getenv("AUTH0_DOMAIN")),
+    "client_id": str(os.getenv("AUTH0_CLIENT_ID")),
+    "client_secret": str(os.getenv("AUTH0_CLIENT_SECRET")),
 }
+fider = {"url": str(os.getenv("FIDER_URL"))}
+slack_api = {"webhook_url": str(os.getenv("SLACK_WEBHOOK_URL"))}
