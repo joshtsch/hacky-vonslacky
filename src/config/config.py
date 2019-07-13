b02a03e2932d35338f9bbf39ac35bf87a4692a7c
Josh Tscheschlog <jtscheschlog@kbra.com>
2019-07-13 13:35:09 -0400

    feat: add webhook url to config

--- src/config/config.py
+++ src/config/config.py
@@ -4,14 +4,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-database = {
-    "driver": str(os.getenv("DB_DRIVER")),
-    "server": str(os.getenv("DB_SERVER")),
-    "name": str(os.getenv("DB_NAME")),
-    "uid": str(os.getenv("DB_UID")),
-    "pwd": str(os.getenv("DB_PWD")),
-    "encrypt": str(os.getenv("DB_ENCRYPT")),
-    "trustServerCert": str(os.getenv("DB_TRUST_SERVER_CERT")),
-    "hostNameInCert": str(os.getenv("DB_HOST_NAME_IN_CERT")),
-    "loginTimeout": os.getenv("DB_LOGIN_TIMEOUT")
+slack_api = {
+    "webhook_url": str(os.getenv("SLACK_WEBHOOK_URL"))
 }
